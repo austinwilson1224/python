a5df5266bdd5e0eefc857ac059a1a9e609e35c4e
austinwilson1224 <austin1224@gmail.com>
2020-07-18 16:50:15 -0700

    started architecture

--- bank-transaction-api/app/app.py
+++ bank-transaction-api/app/app.py
@@ -3,37 +3,85 @@ from flask_restful import Api, Resource
 
 from pymongo import MongoClient
 
+import bcrypt
+
+
+'''
+preliminary stuff
+'''
 app = Flask(__name__)
 api = Api(app)
+
+client = MongoClient("mongodb://db:27017")
+
+db = client.BankAPI
+users = db['Users']
+
+'''
+functions
+'''
+def userExist(username):
+    return users.find({"username":username}[0]).count() != 0
+
+
+
+
+
+'''
+resources
+'''
 
 
 # create a new bank account user 
 class Register(Resource):
     def post(self):
         postedData = request.get_json()
+        username = postedData['username']
+        password = postedData['password']
+        
 
 
 # deposit moneh into the account
 class Add(Resource):
     def post(self):
         postedData = request.get_json()
+        username = postedData['username']
+        password = postedData['password']
+        amount = postedData['amount']
 
 
 # transfer money from one account to another
 class Transfer(Resource):
-    pass
+    def post(self):
+        postedData = request.get_json()
+        username = postedData['username']
+        password = postedData['password']
+        to = postedData['to']
+        amount = postedData['amount']
 
 
-# check the balance of an account
-# should be a get request 
+# check the balance of an account 
 class CheckBalance(Resource):
-    pass
+    def post(self):
+        postedData = request.get_json()
+        username = postedData['username']
+        password = postedData['password']
+
+    
 
 class TakeLoan(Resource):
-    pass
+    def post(self):
+        postedData = request.get_json()
+        username = postedData['username']
+        password = postedData['password']
+        amount = postedData['amount']
 
 class PayLoan(Resource):
-    pass
+    def post(self):
+        postedData = request.get_json()
+        username = postedData['username']
+        password = postedData['password']
+        amount = postedData['amount']
 
 
 
@@ -44,6 +92,11 @@ def hello():
     return "Hello world!"
 
 api.add_resource(Register,'register')
+api.add_resource(Add,'/add')
+api.add_resource(Transfer,'/transfer')
+api.add_resource(CheckBalance,'/balance')
+api.add_resource(TakeLoan,'/takeLoan')
+api.add_resource(PayLoan,'/payLoan')
 
 if __name__ == "__main__":
     app.run('0.0.0.0')
