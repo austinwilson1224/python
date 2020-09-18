6f9bb145feec8814c335ef27db955f20ebecf579
austinwilson1224 <austin1224@gmail.com>
2020-09-18 11:59:20 -0700

    numpy practice

--- num_pie/kaggle.py
+++ num_pie/kaggle.py
@@ -20,3 +20,20 @@ mask = array % 2 == 1
 mask
 array[mask]
 array[array % 2 != 0]
+
+# 5. replace all items that safisfy a condition 
+array = np.arange(10)
+array[array % 2 != 0] = -1
+array
+
+# 6. replace all odd number in  array without changing arr
+array = np.arange(10)
+copy = array.copy()
+copy[copy % 2 != 0] = -1
+copy
+array
+
+# 7. reshape an array -- convert 1D array to 2D array with 2 rows
+array = np.arange(10)
+array
+array.reshape(2,-1)
