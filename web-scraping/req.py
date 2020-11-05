8947a58dd7e9e2d6ee8adb6c738277c333aae978
austinwilson1224 <austin1224@gmail.com>
2020-11-04 19:37:22 -0800

    webscrape test

--- web-scraping/req.py
+++ web-scraping/req.py
@@ -4,7 +4,19 @@ import csv
 
 src = requests.get('https://en.wikipedia.org/wiki/List_of_countries_by_GDP_(PPP)').text
 
+src2 = requests.get('https://www.blowoutcards.com/sports-cards/basketball-cards/2020-21/2020-21-panini-contenders-draft-picks-basketball-hobby-12-box-case.html').text
+
+
+src3 = requests.get('https://www.google.com').text
+
+src4 = requests.get('https://www.blowoutcards.com/2018-topps-wwe-wrestling-hanger-pack-12-box-lot.html').text
+
 soup = BeautifulSoup(src, 'lxml')
+soup2 = BeautifulSoup(src2, 'lxml')
+soup3 = BeautifulSoup(src3, 'lxml')
+soup4 = BeautifulSoup(src4, 'lxml')
+soup4
+
 
 csv_file = open('test.csv', 'w')
 csv_writer = csv.writer(csv_file)
