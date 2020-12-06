33f2b6d2cc72fd412046e33c2800fe1be5a98a18
austinwilson1224 <austin1224@gmail.com>
2020-12-05 19:38:17 -0800

    test

--- 199/eda.py
+++ 199/eda.py
@@ -1,11 +1,12 @@
 import pandas as pd
 
 # importing  the data 
-politifact_fake = pd.read_csv('199/data/politifact_fake.csv')
-politifact_real = pd.read_csv('199/data/politifact_real.csv')
+politifact_fake = pd.read_csv('data/politifact_fake.csv')
+politifact_real = pd.read_csv('data/politifact_real.csv')
 
-gossip_cop = pd.read_csv('199/data/gossipcop_fake.csv')
-gossip_cop_real = pd.read_csv('199/data/gossipcop_real.csv')
+gossip_cop = pd.read_csv('data/gossipcop_fake.csv')
+gossip_cop_real = pd.read_csv('data/gossipcop_real.csv')
+
 
 
 
@@ -13,24 +14,68 @@ gossip_cop_real = pd.read_csv('199/data/gossipcop_real.csv')
 politifact_fake.shape # 432 pieces of news  (fake)
 politifact_real.shape # 624 pieces of news (real)
 
-politifact_fake.columns
-politifact_real.columns
 
+# drop na values 
+politifact_fake.dropna(inplace=True)
+politifact_real.dropna(inplace=True)
+
+# look at shape again 
+politifact_fake.shape
+politifact_real.shape
+
+##################################
+
+cols = politifact_fake.columns
+twitter_stuff = ['user_id', 'tweet_text', 'fake']
+cols = list(cols)
+cols.append(twitter_stuff)
+cols
+
+# make a new data frame to add stuff to
+df = pd.DataFrame(columns=cols)
+
+
+###################################
 # taking a look at the data
 politifact_fake.head()
 politifact_real.head()
 
 
-# number of tweets in the first piece of data 
-len(politifact_fake.iloc[0].tweet_ids.split('\t'))
+
+# storing ids as a list
+politifact_fake['tweet_ids_list'] = politifact_fake.tweet_ids.str.split('\t')
+
+politifact_fake['num_tweets'] = politifact_fake.tweet_ids_list.str.len()
+
+
+
+j = 0
+for i in range(politifact_fake.shape[0]):
+    if pd.isna(politifact_fake.tweet_ids.iloc[i]):
+        continue
+    j += 1
+    temp = politifact_fake.num_tweets.iloc[i]
+    print(temp, end=" ")
+
+j #  392 entries 
+
+
+
+politifact_fake.head()
+
+
 
 # 'id', 'news_url', 'title', 'tweet_ids' 
 
-news_url_fake = politifact_fake.news_url
-len(news_url_fake)
+test_data = politifact_fake.iloc[100]
+test_tweet_id = test_data.tweet_ids_list[0:20]
+# test_tweet_id3 = test_data.tweet_ids_list[3]
 
+test_tweet_id
 
-import tweepy 
+# stuff
+
+import tweepy
 import json 
 
 
@@ -53,6 +98,37 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
+
+
+result = api.statuses_lookup(id_ = test_tweet_id)
+
+for status in result:
+    print("The status " + str(status.id) + " is posted by " + status.user.screen_name) 
+    print("This status says : \n\n" + status.text, end = "\n\n") 
+
+
+
+
+x = json.loads(result[2])
+result[2].expanded_url
+len(result)
+result3
+
+type(result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+########################### old stuff from testing
 
 user = api.get_user("twitter")
 # print(user.screen_name)
